4d122867b4f6eea4f5e6d36d457e85be58c1d528
wrc042 <wrc0326@outlook.com>
2023-05-24 01:10:27 +0800

    merge two editors

--- src/main.cpp
+++ src/main.cpp
@@ -13,27 +13,52 @@ int main() {
 
     auto config = IO::load_config("config.json");
 
-    RLEulerEditor editor;
-    editor.init("liquify editor");
     vector<Color> origin_pixels;
     ImageLoader(config["image"].asString()).load(origin_pixels);
 
-    EulerFluidSolver solver(config["level"].asInt(), config["levelsim"].asInt(),
-                            config["parallel_advection"].asBool());
-    solver.init_image(origin_pixels);
-    solver.set_reset_buffer(
-        [&](const vector<Color> &pixels) { editor.reset_buffer(pixels); });
-    solver.set_update_buffer(
-        [&](const vector<Color> &pixels) { editor.update_buffer(pixels); });
+    if (config["type"].asString() == "euler") {
 
-    const bool &is_closed = editor.is_closed();
-    EulerSolverParam &solver_param = editor.solver_param();
+        RLEulerEditor editor;
+        editor.init("liquify editor");
+
+        EulerFluidSolver solver(config["level"].asInt(),
+                                config["levelsim"].asInt(),
+                                config["parallel_advection"].asBool());
+        solver.init_image(origin_pixels);
+        solver.set_reset_buffer(
+            [&](const vector<Color> &pixels) { editor.reset_buffer(pixels); });
+        solver.set_update_buffer(
+            [&](const vector<Color> &pixels) { editor.update_buffer(pixels); });
+
+        const bool &is_closed = editor.is_closed();
+        EulerSolverParam &solver_param = editor.solver_param();
 #pragma omp parallel sections num_threads(2) default(shared)
-    {
+        {
 #pragma omp section
-        { editor.run(); }
+            { editor.run(); }
 #pragma omp section
-        { solver.run(is_closed, solver_param); }
+            { solver.run(is_closed, solver_param); }
+        }
+    } else if (config["type"].asString() == "classic") {
+        RLClassicEditor editor;
+        editor.init("liquify editor");
+
+        ClassicWarpingSolver solver(config["level"].asInt());
+        solver.init_image(origin_pixels);
+        solver.set_reset_buffer(
+            [&](const vector<Color> &pixels) { editor.reset_buffer(pixels); });
+        solver.set_update_buffer(
+            [&](const vector<Color> &pixels) { editor.update_buffer(pixels); });
+
+        const bool &is_closed = editor.is_closed();
+        ClassicSolverParam &solver_param = editor.solver_param();
+#pragma omp parallel sections num_threads(2) default(shared)
+        {
+#pragma omp section
+            { editor.run(); }
+#pragma omp section
+            { solver.run(is_closed, solver_param); }
+        }
     }
     return 0;
 }
